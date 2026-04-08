4898a8428f0c8028f077855cc4dd9527472d15d2
joshva <joshvadeepa16@gmail.com>
2026-04-08 13:18:25 +0530

    backend

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 import { AuthProvider } from "./app/context/AuthContext.tsx";
 import { Toaster } from 'sonner';
 import { BrowserRouter } from 'react-router';
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AuthProvider>
       <Toaster theme="dark" position="top-center" />
